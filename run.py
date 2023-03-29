e0c154d3ecafceb741c02a04fb833f7e418955d8
Admin <m_gnanasekar@yahoo.in>
2023-03-29 08:53:55 +0530

    run

--- run.py
+++ run.py
@@ -1,3 +1,2 @@
 from simple_salesforce import Salesforce
-sf = Salesforce(username='m_gnanasekar@baxter.com.boost.bstsitf', password='#9Jqsfdxpwd', security_token='TOZCpzUaJT82sR3qVuxcavwn')
- 
+sf = Salesforce(username='m_gnanasekar@baxter.com.boost.bstsitf', password='#9Jqsfdxpw', security_token='TOZCpzUaJT82sR3qVuxcavwn')
